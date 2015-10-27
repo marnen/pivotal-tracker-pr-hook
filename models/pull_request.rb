1dd634c112bab380310aca7628316e341caa8bb8
Marnen Laibow-Koser <mlaibowkoser@rbmtechnologies.com>
2015-10-27 17:03:04 -0400

    Fix indentation.

--- models/pull_request.rb
+++ models/pull_request.rb
@@ -2,11 +2,13 @@ require 'ostruct'
 
 class PullRequest < OpenStruct
   def to_commit
-    {source_commit: {
-      author: user['login'],
-      commit_id: "pulls/#{number}",
-      message: "Created pull request: #{title}",
-      url: html_url
-    }}
+    {
+      source_commit: {
+        author: user['login'],
+        commit_id: "pulls/#{number}",
+        message: "Created pull request: #{title}",
+        url: html_url
+      }
+    }
   end
 end
