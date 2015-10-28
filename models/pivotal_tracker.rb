6009db60f8e15449e14372a427985a7444a17fd3
Marnen Laibow-Koser <mlaibowkoser@rbmtechnologies.com>
2015-10-28 14:37:35 -0400

    Extract methods and remove unused constant.

--- models/pivotal_tracker.rb
+++ models/pivotal_tracker.rb
@@ -3,12 +3,11 @@ require 'net/http'
 
 class PivotalTracker
   BASE_URL = URI 'https://www.pivotaltracker.com/services/v5/'
-  TRACKER_URL = URI 'https://www.pivotaltracker.com/services/v5/source_commits'
 
   def self.story(story_id)
-    story_path = "/stories/#{story_id}"
-    get_request = Net::HTTP::Get.new "#{BASE_URL.path}/#{story_path}".squeeze('/'), headers
-    Net::HTTP::start BASE_URL.host, BASE_URL.port, use_ssl: BASE_URL.scheme == 'https' do |http|
+    story_path = tracker_path "/stories/#{story_id}"
+    get_request = Net::HTTP::Get.new story_path, headers
+    start_tracker_session do |http|
       response = http.request get_request
       response.body ? JSON.parse(response.body) : {}
     end
@@ -22,15 +21,14 @@ class PivotalTracker
     puts "Sending to Pivotal Tracker: #{@pull_request.title}"
     story_paths = @pull_request.story_ids.collect do |story_id|
       project_id = self.class.story(story_id)['project_id']
-      project_id ? "/projects/#{project_id}/stories/#{story_id}" : nil
+      project_id ? self.class.tracker_path("/projects/#{project_id}/stories/#{story_id}/comments") : nil
     end.compact
 
-    Net::HTTP::start BASE_URL.host, BASE_URL.port, use_ssl: BASE_URL.scheme == 'https' do |http|
+    self.class.start_tracker_session do |http|
       story_paths.each do |story_path|
-        post_comment = Net::HTTP::Post.new "#{BASE_URL.path}/#{story_path}/comments".squeeze('/'), self.class.headers
+        post_comment = Net::HTTP::Post.new story_path, self.class.headers
         post_comment.body = @pull_request.to_comment.to_json
-        puts "Path: #{BASE_URL.path}/#{story_path}/comments"
-        puts "Body: #{post_comment.body}"
+        puts "Path: #{story_path}, body: #{post_comment.body}"
         http.request post_comment
       end
     end
@@ -38,10 +36,20 @@ class PivotalTracker
 
   private
 
-  def self.headers
-    @headers ||= {
-      'Content-Type' => 'application/json',
-      'X-TrackerToken' => ENV['PIVOTAL_TRACKER_API_TOKEN']
-    }
+  class << self
+    def start_tracker_session(&block)
+        Net::HTTP::start BASE_URL.host, BASE_URL.port, use_ssl: BASE_URL.scheme == 'https', &block
+    end
+
+    def headers
+      @headers ||= {
+        'Content-Type' => 'application/json',
+        'X-TrackerToken' => ENV['PIVOTAL_TRACKER_API_TOKEN']
+      }
+    end
+
+    def tracker_path(path)
+      "#{BASE_URL.path}/#{path}".squeeze('/')
+    end
   end
 end
